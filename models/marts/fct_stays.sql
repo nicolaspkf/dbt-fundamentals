af99541178314ec48ec9a9c81a676c1a09b28cd8
nicolaspkf <75416079+nicolaspkf@users.noreply.github.com>
2024-05-22 22:48:24 +0200

    Update fct_stays.sql

--- models/marts/fct_stays.sql
+++ models/marts/fct_stays.sql
@@ -6,12 +6,23 @@
     )
 }}
   
--- Load staged stays
+-- Load latest staged data with some overlap to capture late arrivals (late data arrivals - not arrival of customers), assuming that load or ingestion is done at least once per day.
 with stays as (
+    
   select * from {{ ref('stg_source__stays') }}
+  {% if is_incremental() %}
+    where loaded_date >= (select max(loaded_date) from {{this}})
+  {% endif %}
 
 )
 
+with source_data as (
+  select *from {{ source('source_name', 'table_name') }}
+  {% if is_incremental() %}
+    where loaded_date >= current_date - 1
+  {% endif %}
+)
+    
 -- Make calculations and other preparations
 ,final as (
 
